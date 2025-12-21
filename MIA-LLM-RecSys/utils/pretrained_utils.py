0bb11142e16f192da372744f874fa364cc6b77aa
Richard He <103625533+Richardwarriors@users.noreply.github.com>
2025-12-20 23:55:05 -0500

    Add files via upload

---
+++ MIA-LLM-RecSys/utils/pretrained_utils.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import json
+import pickle
+import numpy as np
+import os
+from copy import deepcopy
+
+'''
+def load_ml1m():
+    prompt_sentences = []
+    with open(f"../data/processed_ml-1m/pretrained_ml1m.txt", "r") as prompt_data:
+        for line in prompt_data:
+            prompt_sentences.append(line)
+    return prompt_sentences
+
+def load_beauty():
+    prompt_sentences = []
+    with open(f"../data/Beauty/prompt_Beauty.txt", "r") as prompt_data:
+        for line in prompt_data:
+            prompt_sentences.append(line)
+    return prompt_sentences
+
+def load_book():
+    prompt_sentences = []
+    with open(f"../data/Book/prompt_Book.txt", "r") as prompt_data:
+        for line in prompt_data:
+            prompt_sentences.append(line)
+    return prompt_sentences
+'''
+def load_pretrained_dataset(params):
+    if params["dataset"] == "ml1m":
+        prompt_sentences = load_ml1m()
+        params["prompt_prefix"] = (
+            "You are a recommender system. "
+            "You will receive user–item interaction set derived from the MovieLens-1M dataset. "
+            "Your task is to answer only \"Yes\" or \"No\", indicating whether you have previously seen the user-item interaction set. "
+            "No additional text is allowed.\n\n"
+        )
+        params["task_format"] = "recommendation"
+    elif params["dataset"] == "beauty":
+        prompt_sentences = load_beauty()
+        params[
+            "prompt_prefix"
+        ] = (
+            "You are a recommender system. "
+            "You will receive user–item interaction set derived from the Amazon Beauty dataset. "
+            "Your task is to answer only \"Yes\" or \"No\", indicating whether you have previously seen the user-item interaction set. "
+            "No additional text is allowed.\n\n"
+        )
+        params["task_format"] = "recommendation"
+    elif params["dataset"] == "book":
+        prompt_sentences = load_book()
+        params[
+            "prompt_prefix"
+        ] = (
+            "You are a recommender system. "
+            "You will receive user–item interaction set derived from the Amazon Book dataset. "
+            "Your task is to answer only \"Yes\" or \"No\", indicating whether you have previously seen the user-item interaction set. "
+            "No additional text is allowed.\n\n"
+        )
+        params["task_format"] = "recommendation"
+    else:
+        raise NotImplementedError
+    return prompt_sentences
+
+if __name__ == "__main__":
+    params = {}
+    params["dataset"] = "ml1m"
+    load_pretrained_dataset(params)
+    print(params)
