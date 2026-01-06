86a3e3bde2cef7c1620843acaddbb47bc5891edb
Richard He <103625533+Richardwarriors@users.noreply.github.com>
2026-01-05 23:27:17 -0500

    Update coverage.py

--- MIA-LLM-RecSys/pretrained_MIA/coverage.py
+++ MIA-LLM-RecSys/pretrained_MIA/coverage.py
@@ -2,21 +2,37 @@ import pickle
 import argparse
 
 def main(path, dataset, model):
-    with open(path+dataset+model+'/member.pkl', "rb") as file:
+    with open(path+dataset+'/'+model+'/member.pkl', "rb") as file:
         member_data = pickle.load(file)
-        # Count 1s in member_data
-        member_ones = sum(1 for x in member_data if x == 1)
-        member_total = len(member_data)
-        member_ratio = member_ones / member_total if member_total > 0 else 0
 
-        # Print results
-        print(f"Member Set : 1s = {member_ones}, Total = {member_total}, Ratio = {member_ratio:.4f}")
+    with open(f"../pretrained_RecSys/{dataset}_test.txt", "r") as f:
+        lines = f.readlines()
 
+    # 3. 安全检查（防止长度不一致 silent bug）
+    assert len(member_data) == len(lines), (
+        f"Length mismatch: member_data={len(member_data)}, lines={len(lines)}"
+    )
+
+    res = 0
+
+    for i in range(len(member_data)):
+        member_item = str(member_data[i]).strip()
+        txt_item = lines[i].strip()
+
+        if member_item == txt_item:
+            res += 1
+
+    accuracy = res / len(member_data)
+
+    print(f"Match count: {res}")
+    print(f"Accuracy: {accuracy:.4f}")
+
+    return accuracy
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--path", type =str, default="./results/pretrained/", help="output result path")
-    parser.add_argument("--dataset", type=str, default="beauty", help="dataset")
-    parser.add_argument("--model", type =str, default="/gpt-oss:20b", help="used LLM model")
+    parser.add_argument("--dataset", type=str, default="ml1m", help="dataset")
+    parser.add_argument("--model", type =str, default="llama3", help="used LLM model")
     args = parser.parse_args()
 
     main(args.path, args.dataset, args.model)
